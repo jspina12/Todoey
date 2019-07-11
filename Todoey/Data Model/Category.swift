a96b393bb8a3f5af0d79375a5d51f8624a15677b
John Spina <JohnSpina@johns-mbp-3.lan1>
2019-07-11 15:42:43 -0400

    Persisting data using Realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by John Spina on 7/11/19.
+//  Copyright © 2019 jspina. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+
+  @objc dynamic var name : String = ""
+  let items = List<Item>()
+  
+}
